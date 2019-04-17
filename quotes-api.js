9195f52580fb0ae060567aa90d223cb28c15ba4e
Timmy <timgabrielnguyen@gmail.com>
2019-04-17 16:49:15 -0700

    Server to return all quotes

--- quotes-api.js
+++ quotes-api.js
@@ -4,10 +4,32 @@ const app = express();
 
 const port = 3000;
 
+const quotes = [
+  {
+    id: 1,
+    quote: "The best is yet to come",
+    author: "Unknown",
+    year: 2000
+  },
+  {
+    id: 2,
+    quote: "This is a quote",
+    author: "First last",
+    year: 1930
+  },
+  {
+    id: 3,
+    quote: "This is another quote",
+    author: "First2 Last2",
+    year: 1910
+  }
+];
+
 app.listen(port, function() {
   console.log("Express app listening on port " + port);
 });
 
 app.get("/", function(request, response) {
-  response.send("Get request received at '/'");
+  console.log("Get list of all quotes");
+  response.json(quotes);
 });
